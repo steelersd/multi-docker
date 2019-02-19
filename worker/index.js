28772123b88528a244b0d4bd7c5edc1eb5cb6b22
Adam <steelers_d@hotmail.com>
2019-02-18 19:33:36 -0600

    Copying from Steven's project in hopes the worker project will work properly

--- worker/index.js
+++ worker/index.js
@@ -1,32 +1,19 @@
 const keys = require("./keys");
 const redis = require("redis");
 
-console.log("Working code running...");
-
 const redisClient = redis.createClient({
-	host: keys.redisHost,
-	port: keys.redisPort,
-	retry_strategy: () => 1000
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
 });
 const sub = redisClient.duplicate();
 
-sub.on("error", function(err) {
-	console.log("Something went wrong " + err);
-});
-
-sub.on("connect", function() {
-	hello();
-	console.log("Redis client connected");
-});
-
 function fib(index) {
-	console.log("fib", index);
-	if (index < 2) return 1;
-	return fib(index - 1) + fib(index - 2);
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
 sub.on("message", (channel, message) => {
-	console.log("Message", message);
-	redisClient.hse("values", message, fib(parseInt(message)));
+  redisClient.hset("values", message, fib(parseInt(message)));
 });
 sub.subscribe("insert");
